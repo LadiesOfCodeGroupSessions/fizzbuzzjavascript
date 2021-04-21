92d5526f885a1b1c841ff63b9587fb6627f5a11d
PaolaDM <orchidea.preziosa@gmail.com>
2021-04-21 18:35:10 +0100

    create fizzBuzz 5

--- src/generator.js
+++ src/generator.js
@@ -39,6 +39,18 @@ class Generator {
         }
         return output;
     }
+
+    FizzBuzz5(){
+        let output='';
+        for (let i=1; i<=100; i++){
+            if (i % 3 === 0)
+                output += "Fizz" + '\n';
+            else
+                output += i + '\n';
+        }
+        return output;
+
+    }
 }
 
 module.exports = Generator
--- test/HelloWorld.test.js
+++ test/HelloWorld.test.js
@@ -434,175 +434,72 @@ const expectedOutputFizzBuzz =
       expect(generator.FizzBuzz4()).toBe(expectedOutputFizzBuzz)
     })
 
-// const expectedOutputFizzBuzz =
-//     "1" + '\n' +
-//     "2" + '\n' +
-//     "Fizz" + '\n' +
-//     "4" + '\n' +
-//     "Buzz" + '\n' +
-//     "Fizz" + '\n' +
-//     "7" + '\n' +
-//     "8" + '\n' +
-//     "Fizz" + '\n' +
-//     "Buzz" + '\n' +
-//     "11" + '\n' +
-//     "Fizz" + '\n' +
-//     "13" + '\n' +
-//     "14" + '\n' +
-//     "FizzBuzz" + '\n' +
-//     "16" + '\n' +
-//     "17" + '\n' +
-//     "Fizz" + '\n' +
-//     "19" + '\n' +
-//     "Buzz" + '\n' +
-//     "Fizz" + '\n' +
-//     "22" + '\n' +
-//     "23" + '\n' +
-//     "Fizz" + '\n' +
-//     "Buzz" + '\n' +
-//     "26" + '\n' +
-//     "Fizz" + '\n' +
-//     "28" + '\n' +
-//     "29" + '\n' +
-//     "FizzBuzz" + '\n' +
-//     "31" + '\n' +
-//     "32" + '\n' +
-//     "Fizz" + '\n' +
-//     "34" + '\n' +
-//     "Buzz" + '\n' +
-//     "Fizz" + '\n' +
-//     "37" + '\n' +
-//     "38" + '\n' +
-//     "Fizz" + '\n' +
-//     "Buzz" + '\n' +
-//     "41" + '\n' +
-//     "Fizz" + '\n' +
-//     "43" + '\n' +
-//     "44" + '\n' +
-//     "FizzBuzz" + '\n' +
-//     "46" + '\n' +
-//     "47" + '\n' +
-//     "Fizz" + '\n' +
-//     "49" + '\n' +
-//     "Buzz" + '\n' +
-//     "Fizz" + '\n' +
-//     "52" + '\n' +
-//     "53" + '\n' +
-//     "Fizz" + '\n' +
-//     "Buzz" + '\n' +
-//     "56" + '\n' +
-//     "Fizz" + '\n' +
-//     "58" + '\n' +
-//     "59" + '\n' +
-//     "FizzBuzz" + '\n' +
-//     "61" + '\n' +
-//     "62" + '\n' +
-//     "Fizz" + '\n' +
-//     "64" + '\n' +
-//     "Buzz" + '\n' +
-//     "Fizz" + '\n' +
-//     "67" + '\n' +
-//     "68" + '\n' +
-//     "Fizz" + '\n' +
-//     "70" + '\n' +
-//     "71" + '\n' +
-//     "Fizz" + '\n' +
-//     "73" + '\n' +
-//     "74" + '\n' +
-//     "FizzBuzz" + '\n' +
-//     "76" + '\n' +
-//     "77" + '\n' +
-//     "Fizz" + '\n' +
-//     "79" + '\n' +
-//     "Buzz" + '\n' +
-//     "Fizz" + '\n' +
-//     "82" + '\n' +
-//     "83" + '\n' +
-//     "Fizz" + '\n' +
-//     "Buzz" + '\n' +
-//     "86" + '\n' +
-//     "Fizz" + '\n' +
-//     "88" + '\n' +
-//     "89" + '\n' +
-//     "FizzBuzz" + '\n' +
-//     "91" + '\n' +
-//     "92" + '\n' +
-//     "Fizz" + '\n' +
-//     "94" + '\n' +
-//     "Buzz" + '\n' +
-//     "Fizz" + '\n' +
-//     "97" + '\n' +
-//     "98" + '\n' +
-//     "Fizz" + '\n' +
-//     "Buzz" + '\n'
-
-
-const expectedOutputFizzBuzz =
+const expectedOutputFizzBuzz5 =
     "1" + '\n' +
     "2" + '\n' +
     "Fizz" + '\n' +
     "4" + '\n' +
-    "5" + '\n' +
+    "Buzz" + '\n' +
     "Fizz" + '\n' +
     "7" + '\n' +
     "8" + '\n' +
     "Fizz" + '\n' +
-    "10" + '\n' +
+    "Buzz" + '\n' +
     "11" + '\n' +
     "Fizz" + '\n' +
     "13" + '\n' +
     "14" + '\n' +
-    "Fizz" + '\n' +
+    "FizzBuzz" + '\n' +
     "16" + '\n' +
     "17" + '\n' +
     "Fizz" + '\n' +
     "19" + '\n' +
-    "20" + '\n' +
+    "Buzz" + '\n' +
     "Fizz" + '\n' +
     "22" + '\n' +
     "23" + '\n' +
     "Fizz" + '\n' +
-    "25" + '\n' +
+    "Buzz" + '\n' +
     "26" + '\n' +
     "Fizz" + '\n' +
     "28" + '\n' +
     "29" + '\n' +
-    "Fizz" + '\n' +
+    "FizzBuzz" + '\n' +
     "31" + '\n' +
     "32" + '\n' +
     "Fizz" + '\n' +
     "34" + '\n' +
-    "35" + '\n' +
+    "Buzz" + '\n' +
     "Fizz" + '\n' +
     "37" + '\n' +
     "38" + '\n' +
     "Fizz" + '\n' +
-    "40" + '\n' +
+    "Buzz" + '\n' +
     "41" + '\n' +
     "Fizz" + '\n' +
     "43" + '\n' +
     "44" + '\n' +
-    "Fizz" + '\n' +
+    "FizzBuzz" + '\n' +
     "46" + '\n' +
     "47" + '\n' +
     "Fizz" + '\n' +
     "49" + '\n' +
-    "50" + '\n' +
+    "Buzz" + '\n' +
     "Fizz" + '\n' +
     "52" + '\n' +
     "53" + '\n' +
     "Fizz" + '\n' +
-    "55" + '\n' +
+    "Buzz" + '\n' +
     "56" + '\n' +
     "Fizz" + '\n' +
     "58" + '\n' +
     "59" + '\n' +
-    "Fizz" + '\n' +
+    "FizzBuzz" + '\n' +
     "61" + '\n' +
     "62" + '\n' +
     "Fizz" + '\n' +
     "64" + '\n' +
-    "65" + '\n' +
+    "Buzz" + '\n' +
     "Fizz" + '\n' +
     "67" + '\n' +
     "68" + '\n' +
@@ -612,34 +509,35 @@ const expectedOutputFizzBuzz =
     "Fizz" + '\n' +
     "73" + '\n' +
     "74" + '\n' +
-    "Fizz" + '\n' +
+    "FizzBuzz" + '\n' +
     "76" + '\n' +
     "77" + '\n' +
     "Fizz" + '\n' +
     "79" + '\n' +
-    "80" + '\n' +
+    "Buzz" + '\n' +
     "Fizz" + '\n' +
     "82" + '\n' +
     "83" + '\n' +
     "Fizz" + '\n' +
-    "85" + '\n' +
+    "Buzz" + '\n' +
     "86" + '\n' +
     "Fizz" + '\n' +
     "88" + '\n' +
     "89" + '\n' +
-    "Fizz" + '\n' +
+    "FizzBuzz" + '\n' +
     "91" + '\n' +
     "92" + '\n' +
     "Fizz" + '\n' +
     "94" + '\n' +
-    "95" + '\n' +
+    "Buzz" + '\n' +
     "Fizz" + '\n' +
     "97" + '\n' +
     "98" + '\n' +
     "Fizz" + '\n' +
-    "100" + '\n'
+    "Buzz" + '\n'
+
 
 test('All steps', () => {
   const generator = new Generator()
-  expect(generator.FizzBuzz5()).toBe(expectedOutputFizzBuzz)
+  expect(generator.FizzBuzz5()).toBe(expectedOutputFizzBuzz5)
 })
